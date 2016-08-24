d3f5ca25df7ae382f583694798575c0b3f598db7
raipc <anton-rib@mail.ru>
2016-08-24 19:31:29 +0300

    fixed smallint and bigint types handling

--- src/main/java/com/axibase/tsd/driver/jdbc/enums/AtsdType.java
+++ src/main/java/com/axibase/tsd/driver/jdbc/enums/AtsdType.java
@@ -5,12 +5,14 @@ import org.apache.calcite.avatica.ColumnMetaData.Rep;
 import java.sql.Types;
 
 public enum AtsdType {
+	BIGINT_DATA_TYPE("bigint", "bigint", Types.BIGINT, Rep.LONG, 19),
 	DECIMAL_TYPE("decimal", "decimal", Types.DECIMAL, Rep.OBJECT, -1),
 	DOUBLE_DATA_TYPE("double", "double", Types.DOUBLE, Rep.DOUBLE, 52),
 	FLOAT_DATA_TYPE("float", "float", Types.FLOAT, Rep.FLOAT, 23),
 	INTEGER_DATA_TYPE("integer", "integer", Types.INTEGER, Rep.INTEGER, 10),
 	LONG_DATA_TYPE("long", "bigint", Types.BIGINT, Rep.LONG, 19),
 	SHORT_DATA_TYPE("short", "smallint", Types.SMALLINT, Rep.SHORT, 5),
+	SMALLINT_DATA_TYPE("smallint", "smallint", Types.SMALLINT, Rep.SHORT, 5),
 	STRING_DATA_TYPE("string", "varchar", Types.VARCHAR, Rep.STRING, 2147483647),
 	TIMESTAMP_DATA_TYPE("xsd:dateTimeStamp", "timestamp", Types.TIMESTAMP, Rep.JAVA_SQL_TIMESTAMP,
 			"2016-01-01T00:00:00.000".length());
