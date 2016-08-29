0cffe7dfcc739c59df51ca6fce7b38b4e40bd8b8
raipc <anton-rib@mail.ru>
2016-08-29 19:25:03 +0300

    throw exception on execute(), not next()

--- src/main/java/com/axibase/tsd/driver/jdbc/ext/AtsdRuntimeException.java
+++ src/main/java/com/axibase/tsd/driver/jdbc/ext/AtsdRuntimeException.java
@@ -4,4 +4,12 @@ public class AtsdRuntimeException extends RuntimeException {
 	public AtsdRuntimeException(String message) {
 		super(message);
 	}
+
+	public AtsdRuntimeException(Throwable cause) {
+		super(cause);
+	}
+
+	public AtsdRuntimeException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
