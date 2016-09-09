30b199a0f9c8dbdfbe12b16c464f34cad22e0b25
raipc <anton-rib@mail.ru>
2016-09-09 11:02:39 +0300

    typo

--- src/main/java/com/axibase/tsd/driver/jdbc/enums/timedatesyntax/ArithmeticOperator.java
+++ src/main/java/com/axibase/tsd/driver/jdbc/enums/timedatesyntax/ArithmeticOperator.java
@@ -1,3 +1,17 @@
+/*
+* Copyright 2016 Axibase Corporation or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+* https://www.axibase.com/atsd/axibase-apache-2.0.pdf
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
 package com.axibase.tsd.driver.jdbc.enums.timedatesyntax;
 
 import com.axibase.tsd.driver.jdbc.intf.ITimeDateConstant;
@@ -31,12 +45,12 @@ public enum ArithmeticOperator implements ITimeDateConstant {
 		if (!(secondPrevious instanceof NumberConstant
 				|| secondPrevious instanceof IntervalUnit
 				|| secondPrevious instanceof EndTime)) {
-			throw new IllegalStateException("Addition/substraction can be applied to only Number, IntervalUnit or EndTime constants");
+			throw new IllegalStateException("Addition/subtraction can be applied to only Number, IntervalUnit or EndTime constants");
 		}
 
 		if ((firstPrevious == PLUS || firstPrevious == MINUS)
 				&& (secondPrevious instanceof NumberConstant || secondPrevious instanceof IntervalUnit)) {
-			throw new IllegalStateException("You cannot add or substract raw numbers or intervals");
+			throw new IllegalStateException("You cannot add or subtract raw numbers or intervals");
 		}
 	}
 
