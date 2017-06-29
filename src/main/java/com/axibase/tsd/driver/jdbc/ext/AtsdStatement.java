32e82e58767c9653e898baf767b416a6cb2ecf92
Anton Rybochkin <anton.rybochkin@axibase.com>
2017-06-29 12:03:32 +0300

    fixed Statement#getUpdateCount

--- src/main/java/com/axibase/tsd/driver/jdbc/ext/AtsdStatement.java
+++ src/main/java/com/axibase/tsd/driver/jdbc/ext/AtsdStatement.java
@@ -60,6 +60,16 @@ public class AtsdStatement extends AvaticaStatement {
 	}
 
 	@Override
+	public int getUpdateCount() throws SQLException {
+		return -1;
+	}
+
+	@Override
+	public long getLargeUpdateCount() throws SQLException {
+		return -1L;
+	}
+
+	@Override
 	public synchronized void cancel() throws SQLException {
 		if (!this.cancelFlag.get()) {
 			final AtsdConnection atsdConnection = (AtsdConnection) this.connection;
