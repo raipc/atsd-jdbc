91e918ebbe1e238e61d5b3511268e7c23e9b267f
Anton Rybochkin <anton.rybochkin@axibase.com>
2017-08-10 20:13:13 +0300

    deploy snapshot releases on each commit (#21)

---
+++ .travis-deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+if [[ $JAVA_HOME == *"openjdk"* ]]; then
+    echo "SNAPSHOT DEPLOYING"
+    mvn -Ptravis-deploy-snapshot deploy --settings ./.travis.settings.xml -B -Dgpg.skip
+else
+    echo "SKIP DEPLOYING"
+    mvn verify -B -Dgpg.skip
+fi
